4fe9fb0685cd52999b4a80b3428453f0cdde14bc
wdonet <wdonet@gmail.com>
2015-11-18 11:01:35 +0000

    + changes

--- src/main/java/com/nearsoft/questions/controller/HomeController.java
+++ src/main/java/com/nearsoft/questions/controller/HomeController.java
@@ -1,12 +1,12 @@
 package com.nearsoft.questions.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
-    @RequestMapping("/")
+    @RequestMapping("/home")
     public String home() {
         return "<h3>NS Questions!</h3>";
     }
