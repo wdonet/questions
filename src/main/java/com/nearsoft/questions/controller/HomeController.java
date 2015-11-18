0487873d2df442601f60a0a81db35da0b3085b29
wdonet <wdonet@gmail.com>
2015-11-18 12:14:47 +0000

    1 log and 1 gitignore

--- src/main/java/com/nearsoft/questions/controller/HomeController.java
+++ src/main/java/com/nearsoft/questions/controller/HomeController.java
@@ -1,13 +1,18 @@
 package com.nearsoft.questions.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    private final Logger _log = LoggerFactory.getLogger(getClass());
+
     @RequestMapping("/home")
     public String home() {
+        _log.info("-- showing home --");
         return "<h3>NS Questions!</h3>";
     }
 
