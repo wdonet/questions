ff618863b1130ecbae9763b500692630d96230e7
Israel Gomez <imarban22@gmail.com>
2015-12-15 15:53:39 -0600

    Changing order of tables v1__initial.sql

--- src/main/resources/db/migration/V1__Initial.sql
+++ src/main/resources/db/migration/V1__Initial.sql
@@ -1,28 +1,3 @@
-CREATE TABLE question
-(
-  _id            BIGINT                 NOT NULL,
-  _description   CHARACTER VARYING(255) NOT NULL,
-  _title         CHARACTER VARYING(255) NOT NULL,
-  _total_answers INTEGER                NOT NULL,
-  _user_id       INT8                   NOT NULL,
-  CONSTRAINT question_pkey PRIMARY KEY (_id),
-  CONSTRAINT FK_os8bn3xr2x2owjn69es4hcxgs FOREIGN KEY (_user_id) REFERENCES users
-
-);
-
-CREATE TABLE answer
-(
-  _id           BIGINT                 NOT NULL,
-  _description  CHARACTER VARYING(255) NOT NULL,
-  _question__id BIGINT                 NOT NULL,
-  _user_id      INT8                   NOT NULL,
-  CONSTRAINT answer_pkey PRIMARY KEY (_id),
-  CONSTRAINT FK_ilrlwe1trc8dyqaius89vprop FOREIGN KEY (_user_id) REFERENCES users,
-  CONSTRAINT answer__question_question_fk FOREIGN KEY (_question__id)
-  REFERENCES question (_id) MATCH SIMPLE
-  ON UPDATE NO ACTION ON DELETE NO ACTION
-);
-
 CREATE TABLE profiles
 (
   id         BIGINT NOT NULL,
@@ -30,26 +5,6 @@ CREATE TABLE profiles
   photo_uri  CHARACTER VARYING(255),
   reputation INTEGER,
   CONSTRAINT profiles_pkey PRIMARY KEY (id)
-);
-
-CREATE TABLE tag
-(
-  _id   BIGINT                 NOT NULL,
-  _name CHARACTER VARYING(255) NOT NULL,
-  CONSTRAINT tag_pkey PRIMARY KEY (_id)
-);
-
-CREATE TABLE question__tags
-(
-  question__id BIGINT NOT NULL,
-  _tags__id    BIGINT NOT NULL,
-  CONSTRAINT question__tags_tag_fk FOREIGN KEY (_tags__id)
-  REFERENCES tag (_id) MATCH SIMPLE
-  ON UPDATE NO ACTION ON DELETE NO ACTION,
-  CONSTRAINT question__tags_question_fk FOREIGN KEY (question__id)
-  REFERENCES question (_id) MATCH SIMPLE
-  ON UPDATE NO ACTION ON DELETE NO ACTION,
-  CONSTRAINT question__tags_uk UNIQUE (question__id, _tags__id)
 );
 
 CREATE TABLE userconnection
@@ -87,3 +42,48 @@ CREATE TABLE users
 
 INSERT INTO users (id, email, first_name, last_name, role)
 VALUES (1, 'nsq@nearsoft.com', 'system', 'default', 'ROLE_USER');
+
+CREATE TABLE question
+(
+  _id            BIGINT                 NOT NULL,
+  _description   CHARACTER VARYING(255) NOT NULL,
+  _title         CHARACTER VARYING(255) NOT NULL,
+  _total_answers INTEGER                NOT NULL,
+  _user_id       INT8                   NOT NULL,
+  CONSTRAINT question_pkey PRIMARY KEY (_id),
+  CONSTRAINT FK_os8bn3xr2x2owjn69es4hcxgs FOREIGN KEY (_user_id) REFERENCES users
+
+);
+
+CREATE TABLE answer
+(
+  _id           BIGINT                 NOT NULL,
+  _description  CHARACTER VARYING(255) NOT NULL,
+  _question__id BIGINT                 NOT NULL,
+  _user_id      INT8                   NOT NULL,
+  CONSTRAINT answer_pkey PRIMARY KEY (_id),
+  CONSTRAINT FK_ilrlwe1trc8dyqaius89vprop FOREIGN KEY (_user_id) REFERENCES users,
+  CONSTRAINT answer__question_question_fk FOREIGN KEY (_question__id)
+  REFERENCES question (_id) MATCH SIMPLE
+  ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+CREATE TABLE tag
+(
+  _id   BIGINT                 NOT NULL,
+  _name CHARACTER VARYING(255) NOT NULL,
+  CONSTRAINT tag_pkey PRIMARY KEY (_id)
+);
+
+CREATE TABLE question__tags
+(
+  question__id BIGINT NOT NULL,
+  _tags__id    BIGINT NOT NULL,
+  CONSTRAINT question__tags_tag_fk FOREIGN KEY (_tags__id)
+  REFERENCES tag (_id) MATCH SIMPLE
+  ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT question__tags_question_fk FOREIGN KEY (question__id)
+  REFERENCES question (_id) MATCH SIMPLE
+  ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT question__tags_uk UNIQUE (question__id, _tags__id)
+);
