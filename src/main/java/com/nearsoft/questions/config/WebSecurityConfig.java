4b4f665dda2e4dd32d9fed6e6c299582f0aa2e58
Israel Gomez <imarban22@gmail.com>
2016-01-04 16:26:58 -0600

    Protecting all the site except the home. It fixes the last point of #7

--- src/main/java/com/nearsoft/questions/config/WebSecurityConfig.java
+++ src/main/java/com/nearsoft/questions/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.social.security.SpringSocialConfigurer;
 
 
@@ -18,15 +19,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/**").permitAll()
+                .antMatchers("/css/**").permitAll()
+                .antMatchers("/img/**").permitAll()
+                .antMatchers("/**").fullyAuthenticated()
                 .and()
                 .formLogin()
                 .loginPage("/")
+                .permitAll()
                 .and()
                 .logout()
-                .logoutUrl("/logout")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/")
-                .permitAll().and()
+                .permitAll()
+                .and()
                 .apply(getSpringSocialConfigurer());
     }
 
