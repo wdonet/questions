18c1c8233a56e485170b04ece606230fd157cb06
Israel Gomez <imarban22@gmail.com>
2016-01-04 17:09:27 -0600

    Closes #18. Adding some questions and answers to the flyway scripts.

--- src/main/resources/db/migration/V1.1__Data.sql
+++ src/main/resources/db/migration/V1.1__Data.sql
@@ -20,10 +20,31 @@ INSERT INTO question (_id, _description, _title, _total_answers, _user_id)
 VALUES (4, 'I want to take a PTO tomorrow but I do not know how is supposed to notify or how should I do this',
         'What is the procedure to ask for a PTO?', 1, 1);
 
--- Answers
-
 INSERT INTO question (_id, _description, _title, _total_answers, _user_id)
 VALUES (5, 'Where can I find the quality service document?', 'Where is quality service document?', 1, 1);
+
+INSERT INTO question (_id, _description, _title, _total_answers, _user_id)
+VALUES (6, 'I want to receive the prima payment on the day I will go on vacation',
+        'What is the procedure to receive my prima vacacional?', 1, 1);
+
+INSERT INTO question (_id, _description, _title, _total_answers, _user_id)
+VALUES (7, 'I will take a workshop in Chicago next month. What should I do to request the travel procedure?',
+        'What is the travel expense procedure?', 1, 1);
+
+INSERT INTO question (_id, _description, _title, _total_answers, _user_id)
+VALUES (8, 'DF office sponsored an event. We paid for pizzas and soda. What do I need to send you for reimbursement
+            of the expense',
+        'How can I request an invoice payment?', 1, 1);
+
+INSERT INTO question (_id, _description, _title, _total_answers, _user_id)
+VALUES (9, 'In my previous job I did need to do tax declaration. Why I need to do it here?',
+        'Why do I need to do tax declaration?', 1, 1);
+
+INSERT INTO question (_id, _description, _title, _total_answers, _user_id)
+VALUES (10, 'I want to do my tax declaration online but I need my SAT password. How can I get it?',
+        'How can I get my SAT password?', 1, 1);
+
+-- Answers
 
 INSERT INTO answer (_id, _description, _question__id, _user_id)
 VALUES (1, 'https://docs.google.com/document/d/1JbMiYhO4W7eqolhdUqOAlRSgPEN3t7X3WsRweJOPE-w/edit#', 1, 1);
@@ -47,6 +68,22 @@ This document defines the expectation and the quality of service that the differ
 Nearsoft and to organize our workflow.\n Please keep it reachable and feel free to read it every time to
 have a question, so you can manage your timeframes.',
         5, 1);
+
+INSERT INTO answer (_id, _description, _question__id, _user_id)
+VALUES (5, 'In this document you can look for the answers about the Vacation Payment Procedure. https://goo.gl/8H1iI2',
+        6, 1);
+
+INSERT INTO answer (_id, _description, _question__id, _user_id)
+VALUES (6, 'You can check the Travel Expenses Procedure document to know how to proceed with your travel. Good luck. https://goo.gl/p3cm8P',
+        7, 1);
+
+INSERT INTO answer (_id, _description, _question__id, _user_id)
+VALUES (7, 'Please check the Expenses Procedure Document. It contains the answer for this question. https://goo.gl/odIuU9',
+        8, 1);
+
+INSERT INTO answer (_id, _description, _question__id, _user_id)
+VALUES (8, 'You can use this link https://goo.gl/aao8IE for that',
+        10, 1);
 
 -- Tags
 
@@ -76,3 +113,22 @@ VALUES (4, 3);
 
 INSERT INTO question__tags (question__id, _tags__id)
 VALUES (5, 1);
+
+INSERT INTO question__tags (question__id, _tags__id)
+VALUES (6, 1);
+
+INSERT INTO question__tags (question__id, _tags__id)
+VALUES (6, 3);
+
+INSERT INTO question__tags (question__id, _tags__id)
+VALUES (7, 1);
+
+INSERT INTO question__tags (question__id, _tags__id)
+VALUES (8, 1);
+
+INSERT INTO question__tags (question__id, _tags__id)
+VALUES (9, 1);
+
+INSERT INTO question__tags (question__id, _tags__id)
+VALUES (10, 1);
+
