1f49668fa941a1f5525cd3b185ba4b47134d1764
Israel Gomez <imarban22@gmail.com>
2016-01-12 13:20:59 -0600

    Closes #29. It changes the name of all the properties with underscore

--- src/main/java/com/nearsoft/questions/repository/TagRepository.java
+++ src/main/java/com/nearsoft/questions/repository/TagRepository.java
@@ -1,12 +1,13 @@
 package com.nearsoft.questions.repository;
 
-import java.util.List;
 import com.nearsoft.questions.domain.Tag;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TagRepository extends CrudRepository<Tag, Long> {
 
-    List<Tag> findBy_nameIn(List<String> commaDelimitedTagNameList);
+    List<Tag> findByNameIn(List<String> commaDelimitedTagNameList);
 }
