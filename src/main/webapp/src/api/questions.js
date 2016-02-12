92817af2e215533b1b9c2d9a97e938d9b27e4c9a
Alejandro Moreno <amoreno@nearsoft.com>
2016-02-11 20:53:19 -0700

    Adding example of integration of api with reactjs (not really a good implementation, it's just for testing purposes, everything is dirty).

--- src/main/webapp/src/api/questions.js
+++ src/main/webapp/src/api/questions.js
@@ -1,50 +1,19 @@
 import { Router } from 'express';
+import fetch from '../core/fetch';
 
 const router = new Router();
 
 router.get('/', async (req, res, next) => {
-  const questions = [
-    {
-      id: 1,
-      title: 'Question 1 test',
-      description: 'Description description description',
-      totalAnswers: 21,
-    },
-    {
-      id: 2,
-      title: 'Question 2 test',
-      description: 'Description description description',
-      totalAnswers: 22,
-    },
-    {
-      id: 3,
-      title: 'Question 3 test',
-      description: 'Description description description',
-      totalAnswers: 23,
-    },
-    {
-      id: 4,
-      title: 'Question 4 test',
-      description: 'Description description description',
-      totalAnswers: 24,
-    },
-    {
-      id: 5,
-      title: 'Question 5 test',
-      description: 'Description description description',
-      totalAnswers: 25,
-    },
-  ];
-
   try {
     const order = req.query.order;
 
-    if (!order || order === 'undefined') {
-      res.status(200).send({ questions });
+    if (order !== 'undefined' && order) {
+      var response = await fetch('http://localhost:8080/api/questions?order=' + order);
     } else {
-      questions.reverse();
-      res.status(200).send({ questions });
+      var response = await fetch('http://localhost:8080/api/questions');
     }
+
+    res.status(200).send(await response.json());
   } catch (err) {
     next(err);
   }
