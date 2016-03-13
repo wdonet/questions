1e8fd73b115723491227f0497b81237c6302e327
Cesar Terrazas <cesar.terrazas.g@gmail.com>
2016-03-13 11:01:56 -0700

    Remove unused imports

--- src/main/java/com/nearsoft/questions/controller/HomeController.java
+++ src/main/java/com/nearsoft/questions/controller/HomeController.java
@@ -1,18 +1,10 @@
 package com.nearsoft.questions.controller;
 
-import com.nearsoft.questions.domain.Question;
-import com.nearsoft.questions.domain.Tag;
-import com.nearsoft.questions.domain.auth.User;
-import com.nearsoft.questions.service.QuestionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.Arrays;
 
 @Controller
 public class HomeController {
