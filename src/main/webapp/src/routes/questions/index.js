d785661c75fe14273672fd969f924609285498db
eduarbo <eduarbo@gmail.com>
2016-04-26 13:33:09 -0700

    fetch questions from api

--- src/main/webapp/src/routes/questions/index.js
+++ src/main/webapp/src/routes/questions/index.js
@@ -7,21 +7,9 @@ export default {
   path: '/questions',
 
   async action() {
-    // const response = await fetch('localhost:8080/api/questions');
-    // if (response.status !== 200) throw new Error(response.statusText);
-    // const content = await response.json();
-    // return content && <Questions {...content._embedded} />;
-    const content = {
-      questions: [
-        {
-          id: 666,
-          title: 'test',
-          totalAnswers: 10,
-        },
-      ],
-    };
-
-    return <Questions { ...content } />;
+    const response = await fetch('/api/questions');
+    if (response.status !== 200) throw new Error(response.statusText);
+    const { _embedded } = await response.json();
+    return content && <Questions {..._embedded} />;
   },
-
 };
