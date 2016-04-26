4ee1ba10b410c1e53b9bed6c1e5ce17be8b0ba51
eduarbo <eduarbo@gmail.com>
2016-04-26 12:27:22 -0700

    upgrade reactkit to v0.5

--- src/main/webapp/src/server.js
+++ src/main/webapp/src/server.js
@@ -7,60 +7,96 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-import 'babel-core/polyfill';
+import 'babel-polyfill';
 import path from 'path';
 import express from 'express';
-import React from 'react';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
 import ReactDOM from 'react-dom/server';
-import Router from './routes';
-import Html from './components/Html';
+import { match } from 'universal-router';
+import PrettyError from 'pretty-error';
+import routes from './routes';
 import assets from './assets';
-import { port } from './config';
+import { port, auth, analytics } from './config';
 
-const server = global.server = express();
+const app = express();
+
+//
+// Tell any CSS tooling (such as Material UI) to use all vendor prefixes if the
+// user agent is not known.
+// -----------------------------------------------------------------------------
+global.navigator = global.navigator || {};
+global.navigator.userAgent = global.navigator.userAgent || 'all';
 
 //
 // Register Node.js middleware
 // -----------------------------------------------------------------------------
-server.use(express.static(path.join(__dirname, 'public')));
-
-//
-// Register API middleware
-// -----------------------------------------------------------------------------
-server.use('/api/content', require('./api/content'));
-server.use('/api/questions', require('./api/questions'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 //
 // Register server-side rendering middleware
 // -----------------------------------------------------------------------------
-server.get('*', async (req, res, next) => {
+app.get('*', async (req, res, next) => {
   try {
+    let css = [];
     let statusCode = 200;
+    const template = require('./views/index.jade');
     const data = { title: '', description: '', css: '', body: '', entry: assets.main.js };
-    const css = [];
-    const context = {
-      insertCss: styles => css.push(styles._getCss()),
-      onSetTitle: value => data.title = value,
-      onSetMeta: (key, value) => data[key] = value,
-      onPageNotFound: () => statusCode = 404,
-    };
 
-    await Router.dispatch({ path: req.path, query: req.query, context }, (state, component) => {
-      data.body = ReactDOM.renderToString(component);
-      data.css = css.join('');
+    if (process.env.NODE_ENV === 'production') {
+      data.trackingId = analytics.google.trackingId;
+    }
+
+    await match(routes, {
+      path: req.path,
+      query: req.query,
+      context: {
+        insertCss: styles => css.push(styles._getCss()),
+        setTitle: value => (data.title = value),
+        setMeta: (key, value) => (data[key] = value),
+      },
+      render(component, status = 200) {
+        css = [];
+        statusCode = status;
+        data.body = ReactDOM.renderToString(component);
+        data.css = css.join('');
+        return true;
+      },
     });
 
-    const html = ReactDOM.renderToStaticMarkup(<Html {...data} />);
-    res.status(statusCode).send('<!doctype html>\n' + html);
+    res.status(statusCode);
+    res.send(template(data));
   } catch (err) {
     next(err);
   }
 });
 
 //
+// Error handling
+// -----------------------------------------------------------------------------
+const pe = new PrettyError();
+pe.skipNodeFiles();
+pe.skipPackage('express');
+
+app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+  console.log(pe.render(err)); // eslint-disable-line no-console
+  const template = require('./views/error.jade');
+  const statusCode = err.status || 500;
+  res.status(statusCode);
+  res.send(template({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? '' : err.stack,
+  }));
+});
+
+//
 // Launch the server
 // -----------------------------------------------------------------------------
-server.listen(port, () => {
-  /* eslint-disable no-console */
+/* eslint-disable no-console */
+app.listen(port, () => {
   console.log(`The server is running at http://localhost:${port}/`);
 });
+/* eslint-enable no-console */
