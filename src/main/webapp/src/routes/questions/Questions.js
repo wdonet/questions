1e5134f3817dd012b53eb85adb9f0a21b45c467e
eduarbo <eduarbo@gmail.com>
2016-04-28 14:32:30 -0700

    Split questions items into a component

--- src/main/webapp/src/routes/questions/Questions.js
+++ src/main/webapp/src/routes/questions/Questions.js
@@ -1,39 +1,25 @@
 import React, { PropTypes } from 'react';
 import cx from 'classnames';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import Link from '../../components/Link';
 import s from './Questions.scss';
+import QuestionCard from '../../components/QuestionCard';
 
 const title = 'Questions';
 
 function Questions({ questions, className }, context) {
   context.setTitle(title);
+
+  const questionsList = questions.map(function renderQuestions(question) {
+    return <QuestionCard key={question._links.self.href} {...question} />;
+  });
+
   return (
-    <div className={cx(s.container, className)}>
-      <h1 className={s.unansweredQuestionsTitle}>
-        <img src={require('./question-icon.png')} className={s.questionIcon} />
-        Questions
-      </h1>
-      <ul className={s.suggestionsCont}>
-        {questions.map(function renderQuestions(question) {
-          return (
-              <li key={question._links.self.href}>
-              <Link className={s.answerTitle} to={`/questions/${question.id}`}>
-                {question.title}
-              </Link>
-              <div className={s.answersTotal}>- {question.totalAnswers} Answers</div>
-              <div className={s.tagsCont}>
-                <div className={s.tagIcon}>
-                  <img src={require('./tag-icon.png')} alt="tags" />
-                  Tags:
-                </div>
-                <span className={s.tags}>finance</span>
-              </div>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <section className={cx(s.root, className)}>
+      <h1 className={s.title}>Questions</h1>
+      <section className={s.content}>
+        {questionsList}
+      </section>
+    </section>
   );
 }
 
