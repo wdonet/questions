efd38d3dd99f229fed71ba66837862b82eef93d6
eduarbo <eduarbo@gmail.com>
2016-04-27 21:49:18 -0700

    Improve styling for home and add Feature component

--- src/main/webapp/src/routes/home/Home.js
+++ src/main/webapp/src/routes/home/Home.js
@@ -1,32 +1,27 @@
 import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Home.scss';
+import Feature from '../../components/Feature';
 
 const title = 'NSQuestions';
 
 function Home(props, context) {
   context.setTitle(title);
   return (
-    <div className={s.container}>
+    <div className={s.root}>
       <h1 className={s.title}>
-        It's easy to find answers to <span className={s.highlight}>internal</span>  &amp; <span className={s.highlight}>technical</span> questions
+        It's easy to find answers to <strong>internal</strong>  &amp; <strong>technical</strong> questions
       </h1>
-      <section className={s.howItWorks}>
-        <div className={s.howCont}>
-          <img src={require('./busca-img.png')} alt="Busca alguna pregunta" />
-          <h1>Search Questions</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
-        </div>
-        <div className={s.howCont}>
-          <img src={require('./crea-img.png')} alt="Crea tu pregunta" />
-          <h1>Create Questions</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
-        </div>
-          <div className={s.howCont}>
-          <img src={require('./notifica-img.png')} alt="Notifica" />
-          <h1>Notify Questions</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
-        </div>
+      <section className={s.features}>
+        <Feature title="Search Questions" img={require('./busca-img.png')} imgAlt="Busca alguna pregunta">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.
+        </Feature>
+        <Feature title="Create Questions" img={require('./crea-img.png')} imgAlt="Crea tu pregunta">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.
+        </Feature>
+        <Feature title="Notify Questions" img={require('./notifica-img.png')} imgAlt="Notifica">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.
+        </Feature>
       </section>
     </div>
   );
