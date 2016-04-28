132f1128ca51517c711c1a7f97ecbb538238dc9e
eduarbo <eduarbo@gmail.com>
2016-04-28 14:32:30 -0700

    Split questions items into a component

---
+++ src/main/webapp/src/components/QuestionCard/QuestionCard.js
@@ -0,0 +1,38 @@
+import React, { PropTypes } from 'react';
+import cx from 'classnames';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import Link from '../../components/Link';
+import s from './QuestionCard.scss';
+
+function QuestionCard({ title, totalAnswers, tags = [], user, className }, context) {
+  const tagsList = tags.map(function renderTags(tag) {
+    return <li key={tag} className={s.tag}><Link to="/">{tag}</Link></li>;
+  });
+
+  return (
+    <article className={cx(s.root, className)}>
+      <header>
+        <h1 className={s.title}><Link to="/">{title}</Link></h1>
+        <span className={s.totalAnswers}>- {totalAnswers} Answers</span>
+      </header>
+
+      <footer>
+        <strong className={s.tagsHeader}>Tags:</strong>
+        <ul className={s.tagsList}>
+          {tagsList}
+        </ul>
+        <b className={s.author}><small>Asked by</small> {user.fullName}</b>
+      </footer>
+    </article>
+  );
+}
+
+QuestionCard.propTypes = {
+  className: PropTypes.string,
+  title: PropTypes.string,
+  totalAnswers: PropTypes.number,
+  tags: PropTypes.array,
+  user: PropTypes.object,
+};
+
+export default withStyles(s)(QuestionCard);
