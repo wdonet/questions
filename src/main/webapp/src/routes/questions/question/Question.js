2ef6dde60f89436d5f392919b38cbd481ceee58d
eduarbo <eduarbo@gmail.com>
2016-05-03 16:43:37 -0700

    Improve question component

--- src/main/webapp/src/routes/questions/question/Question.js
+++ src/main/webapp/src/routes/questions/question/Question.js
@@ -2,14 +2,32 @@ import React, { PropTypes } from 'react';
 import cx from 'classnames';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Question.scss';
+import TagList from '../../../components/TagList';
 
 function Question(props, context) {
-  context.setTitle(props.title);
+  const { title, tags, description, user, className } = props;
+
+  context.setTitle(title);
 
   return (
-    <section className={cx(s.root, props.className)}>
-      {props.title}
-    </section>
+    <article className={cx(s.root, className)}>
+      <header>
+        <h1 className={s.title}>{title}</h1>
+        <TagList tags={tags} />
+      </header>
+
+      <section className={s.description}>
+        {description}
+      </section>
+
+      <footer className={s.footer}>
+        asked by {user.fullName}
+      </footer>
+
+      <section className={s.answers}>
+        <h2>What people have answered</h2>
+      </section>
+    </article>
   );
 }
 
@@ -20,6 +38,7 @@ Question.contextTypes = {
 Question.propTypes = {
   className: PropTypes.string,
   description: PropTypes.string,
+  tags: PropTypes.array,
   title: PropTypes.string,
   totalAnswers: PropTypes.number,
   user: PropTypes.object,
