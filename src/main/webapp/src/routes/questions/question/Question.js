69e5fc351b2569f426090c0995b68a6ae08af144
eduarbo <eduarbo@gmail.com>
2016-05-04 17:47:58 -0700

    Complete Question component

--- src/main/webapp/src/routes/questions/question/Question.js
+++ src/main/webapp/src/routes/questions/question/Question.js
@@ -3,17 +3,21 @@ import cx from 'classnames';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Question.scss';
 import TagList from '../../../components/TagList';
+import Answer from '../../../components/Answer';
 
 function Question(props, context) {
-  const { title, tags, description, user, className } = props;
+  const { title, tags, description, user, answers, className } = props;
 
   context.setTitle(title);
+
+  const answerList = answers.length ? answers.map(answer =>
+    <Answer className={s.answer} {...answer} />) : 'No answers';
 
   return (
     <article className={cx(s.root, className)}>
       <header>
         <h1 className={s.title}>{title}</h1>
-        <TagList tags={tags} />
+        {!!tags.length && <TagList tags={tags} />}
       </header>
 
       <section className={s.description}>
@@ -21,11 +25,20 @@ function Question(props, context) {
       </section>
 
       <footer className={s.footer}>
-        asked by {user.fullName}
+        <b className={s.author}>asked by {user.fullName}</b>
       </footer>
 
       <section className={s.answers}>
-        <h2>What people have answered</h2>
+        <h3>What people have answered</h3>
+        {answerList}
+
+        <section className={s.addAnswer}>
+          <h3>Add an answer</h3>
+          <textarea className={s.answerInput}
+            placeholder="Add a detailed answer"
+          />
+          <button className={cx(s.addButton, 'blue')}>Add</button>
+        </section>
       </section>
     </article>
   );
@@ -39,9 +52,15 @@ Question.propTypes = {
   className: PropTypes.string,
   description: PropTypes.string,
   tags: PropTypes.array,
+  answers: PropTypes.array,
   title: PropTypes.string,
   totalAnswers: PropTypes.number,
   user: PropTypes.object,
+};
+
+Question.defaultProps = {
+  answers: [],
+  tags: [],
 };
 
 export default withStyles(s)(Question);
