a69cc75fca9cdfd4f324e18b309c87a44e098f84
eduarbo <eduarbo@gmail.com>
2016-05-06 16:28:45 -0700

    Add missing propTypes to Feature component

--- src/main/webapp/src/components/Feature/Feature.js
+++ src/main/webapp/src/components/Feature/Feature.js
@@ -2,14 +2,23 @@ import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Feature.scss';
 
-function Feature({title, img, imgSrc, children}, context) {
+function Feature({ title, img, imgAlt, children }) {
   return (
     <section className={s.root}>
-      <img className={s.image} src={img} alt={imgSrc} />
+      <img className={s.image} src={img} alt={imgAlt} />
       <h1 className={s.title}>{title}</h1>
-      <p className={s.content}>{children}</p>
+      <p className={s.content}>
+        {children}
+      </p>
     </section>
   );
 }
+
+Feature.propTypes = {
+  title: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
+  imgAlt: PropTypes.string.isRequired,
+  children: PropTypes.string.isRequired,
+};
 
 export default withStyles(s)(Feature);
