04df317a4e47e5997887e937d91dfe5e975cf88d
eduarbo <eduarbo@gmail.com>
2016-05-07 10:20:54 -0700

    Fix linting errors

--- src/main/webapp/src/routes/home/Home.js
+++ src/main/webapp/src/routes/home/Home.js
@@ -10,16 +10,23 @@ function Home(props, context) {
   return (
     <section className={s.root}>
       <h1 className={s.title}>
-        It's easy to find answers to <strong className={s.hl}>internal</strong> &amp; <strong className={s.hl}>technical</strong> questions
+        It's easy to find answers to <strong className={s.hl}>internal</strong>
+         &amp; <strong className={s.hl}>technical</strong> questions
       </h1>
       <div className={s.features}>
-        <Feature title="Search Questions" img={require('./busca-img.png')} imgAlt="Busca alguna pregunta">
+        <Feature title="Search Questions"
+          img={require('./busca-img.png')} imgAlt="Busca alguna pregunta"
+        >
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.
         </Feature>
-        <Feature title="Create Questions" img={require('./crea-img.png')} imgAlt="Crea tu pregunta">
+        <Feature title="Create Questions"
+          img={require('./crea-img.png')} imgAlt="Crea tu pregunta"
+        >
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.
         </Feature>
-        <Feature title="Notify Questions" img={require('./notifica-img.png')} imgAlt="Notifica">
+        <Feature title="Notify Questions"
+          img={require('./notifica-img.png')} imgAlt="Notifica"
+        >
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.
         </Feature>
       </div>
