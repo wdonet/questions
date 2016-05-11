b3ed6a61c66772a1de8b58bc9c780aa4dcc74ac5
cterrazas <cterrazas@gmail.com>
2016-05-11 16:14:22 -0600

    Ignore a unit test that requires a connection with the DB

--- src/test/java/com/nearsoft/QuestionsApplicationTests.java
+++ src/test/java/com/nearsoft/QuestionsApplicationTests.java
@@ -1,12 +1,14 @@
 package com.nearsoft;
 
 import com.nearsoft.questions.config.QuestionsApplication;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@Ignore//Requires a DB
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = QuestionsApplication.class)
 @WebAppConfiguration
