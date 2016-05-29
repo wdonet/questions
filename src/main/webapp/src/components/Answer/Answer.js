26230e60012b7d7ce2a6f3abe5d22dd6869e5ae8
eduarbo <eduarbo@gmail.com>
2016-05-29 15:38:45 -0500

    Update styling for Question route

--- src/main/webapp/src/components/Answer/Answer.js
+++ src/main/webapp/src/components/Answer/Answer.js
@@ -6,13 +6,12 @@ import s from './Answer.scss';
 function Answer({ description, user, className }) {
   return (
     <article className={cx(s.root, className)}>
+      <footer className={s.footer}>
+        Answered By <span className={s.author}>{user.fullName}</span>
+      </footer>
       <section className={s.description}>
         {description}
       </section>
-
-      <footer className={s.footer}>
-        <b className={s.author}>answered by {user.fullName}</b>
-      </footer>
     </article>
   );
 }
