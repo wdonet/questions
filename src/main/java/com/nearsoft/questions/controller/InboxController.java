82a45936542c398427674f3b1f15ea551c77462c
wdonet <wdonet@gmail.com>
2016-05-31 14:17:17 -0700

    Inbox controller

---
+++ src/main/java/com/nearsoft/questions/controller/InboxController.java
@@ -0,0 +1,16 @@
+package com.nearsoft.questions.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/inbox")
+public class InboxController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String inboxHome() {
+        return "inbox";
+    }
+
+}
