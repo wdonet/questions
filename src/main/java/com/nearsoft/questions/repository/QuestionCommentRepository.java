9acf2cb7a7c1ea00aa8095874d4a8dc3c9441b53
vanchondo <vanchondo@nearsoft.com>
2016-06-01 00:03:08 -0500

    Added question comment save
    Added repository to save question comments.
    Added dummy form to save question comments at
    /comments/question/{question_id}

---
+++ src/main/java/com/nearsoft/questions/repository/QuestionCommentRepository.java
@@ -0,0 +1,9 @@
+package com.nearsoft.questions.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.nearsoft.questions.domain.QuestionComment;
+
+public interface QuestionCommentRepository extends CrudRepository<QuestionComment, Long> {
+
+}
