a77e06f883e53c2cb900a7292aee26dccae6a6d9
Israel Gomez <imarban22@gmail.com>
2016-06-01 10:33:52 -0500

    Notifications controller endpoints

--- src/main/java/com/nearsoft/questions/controller/NotificationsController.java
+++ src/main/java/com/nearsoft/questions/controller/NotificationsController.java
@@ -12,7 +12,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
 
@@ -31,22 +30,27 @@ public class NotificationsController extends BaseController {
     private NotificationRepository repository;
 
     @RequestMapping(method = RequestMethod.GET)
+    public String inbox(){
+        return "inbox";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/notifications")
     public List<Notification> getAll(Model model, @AuthenticationPrincipal UserDetails activeUser) {
         model.addAttribute("notifications", repository.findByUser(activeUser.getUser()));
         return repository.findByUser(activeUser.getUser());
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, path = "/{id}")
+    @RequestMapping(method = RequestMethod.DELETE, path = "/notifications/{id}")
     public void delete(@PathVariable Long id) {
         repository.delete(id);
     }
 
-    @RequestMapping(method = RequestMethod.PUT, path = "/{id}")
+    @RequestMapping(method = RequestMethod.PUT, path = "/notifications/read/{id}")
     public void read(@PathVariable Long id) {
         repository.delete(id);
     }
 
-    @RequestMapping(method = RequestMethod.PUT, path = "/")
+    @RequestMapping(method = RequestMethod.POST, path = "/notifications/read")
     public void readAll(@AuthenticationPrincipal UserDetails activeUser) {
         List<Notification> notifications = repository.findByUser(activeUser.getUser());
         notifications.forEach(notification -> notification.setUiNotified(true));
