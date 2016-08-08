b54fbb4e35670a927c5a5880d7f88f1b9763e947
wdonet <wdonet@gmail.com>
2016-08-07 23:52:33 -0700

    application properties adjustments

--- src/main/java/com/nearsoft/questions/controller/GlobalExceptionHandler.java
+++ src/main/java/com/nearsoft/questions/controller/GlobalExceptionHandler.java
@@ -2,28 +2,29 @@ package com.nearsoft.questions.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 
-//@ControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
     public static final String DEFAULT_ERROR_VIEW = "error";
 
-    private final Logger _log = LoggerFactory.getLogger(getClass());
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     @ExceptionHandler(IllegalStateException.class)
     public ModelAndView handleIllegalStateException(HttpServletRequest req, Exception exception) {
-        _log.error("Error : " + exception.getMessage());
-        return buildModelAndView(DEFAULT_ERROR_VIEW, "Session", exception.getMessage(), exception.getStackTrace());
+        log.error("Error : " + exception.getMessage());
+        return buildModelAndView(DEFAULT_ERROR_VIEW, "Session", exception);
     }
 
     @ExceptionHandler(Exception.class)
     public ModelAndView handleDefaultException(HttpServletRequest req, Exception exception) {
-        _log.error("Error : " + exception.getMessage());
+        log.error("Error : " + exception.getMessage());
         //todo wdonet : If the exception is annotated with @ResponseStatus rethrow it and
         // let the framework handle it - like:
 //        @ResponseStatus(value=HttpStatus.NOT_FOUND, reason="No such Order")  // 404
@@ -31,15 +32,22 @@ public class GlobalExceptionHandler {
 //        }
 //        if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null)
 //            throw e;
-        return buildModelAndView(DEFAULT_ERROR_VIEW, "General", exception.getMessage(), exception.getStackTrace());
+        return buildModelAndView(DEFAULT_ERROR_VIEW, "General", exception);
     }
 
-    private ModelAndView buildModelAndView(String viewName, String status, String message, StackTraceElement... stackTrace) {
+    private ModelAndView buildModelAndView(String viewName, String status, Exception e) {
         ModelAndView mav = new ModelAndView(viewName);
         mav.addObject("status", status);
+        String message = e.getMessage();
+        if (message == null && e.getCause() != null) {
+            message = e.getCause().getCause().getMessage();
+        }
+        if (message == null) {
+            message = e.toString();
+        }
         mav.addObject("message", message);
-        if (_log.isDebugEnabled()) {
-            mav.addObject("stackTrace", Arrays.toString(stackTrace));
+        if (log.isDebugEnabled()) {
+            mav.addObject("stackTrace", Arrays.toString(e.getStackTrace()));
         }
         return mav;
     }
--- src/main/java/com/nearsoft/questions/repository/search/QuestionSearchRepository.java
+++ src/main/java/com/nearsoft/questions/repository/search/QuestionSearchRepository.java
@@ -1,19 +1,25 @@
 package com.nearsoft.questions.repository.search;
 
+import java.util.List;
 import com.nearsoft.questions.domain.Question;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface QuestionSearchRepository extends ElasticsearchRepository<Question, Long> {
 
     Page<Question> findByTitleOrDescription(String title, String description, Pageable pageable);
 
-    List<Question> findByDescriptionLike(String description);
+    Page<Question> findByTagsNameIn(List<String> tags, Pageable pageable); //todo should rename each tag to '*tag*'
 
+//    @Query("{\"bool\" : {\"should\" : [" +
+//        " {\"field\" : {\"title\" : \"?0\"}}" +
+//        " {\"field\" : {\"description\" : \"?0\"}}," +
+//        " {\"field\" : {\"answers.description\" : \"?0\"}}" +
+//        " ]}}")
+//    Page<Question> findSmt(String qry, Pageable pageable);
 
 }
