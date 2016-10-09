c394c0737dd176ae0d378e46aa6d881df141bca0
wdonet <wdonet@gmail.com>
2016-10-08 23:33:49 -0700

    sql to clean initial data

---
+++ tools/clearInitialData.sql
@@ -0,0 +1,4 @@
+DELETE FROM question_tags WHERE question_id IN (1,2,3,4,5,6,7,8,9,10);
+DELETE FROM tag WHERE id in (1,2,3,4,5,6);
+DELETE FROM answer WHERE id IN (1,2,3,4,5,6,7,8);
+DELETE FROM question WHERE id IN (1,2,3,4,5,6,7,8,9,10);
